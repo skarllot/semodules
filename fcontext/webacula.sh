40f969e929b44a2ba6a3ff7455f9992ecf2be0e2
Fabrício Godoy <skarllot@gmail.com>
2014-01-07 16:31:12 -0200

    Fixed webacula fcontext script

--- fcontext/webacula.sh
+++ fcontext/webacula.sh
@@ -4,5 +4,6 @@ BASE_DIR=/var/www/webacula
 
 semanage fcontext -a -t httpd_cache_t "${BASE_DIR}/data/cache(/.*)?"
 restorecon -R -v ${BASE_DIR}/data/cache
+chown -R apache. ${BASE_DIR}/data/cache
 
 chmod -R o-w ${BASE_DIR}
